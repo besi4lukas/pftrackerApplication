5d825c801f450f64a5be81ed65cc9df10fc6071d
kbesidonne <kbesidonne@expediagroup.com>
2023-06-18 15:20:59 -0500

    initial commit

---
+++ src/main/java/com/pftracker/pftrackerApplication/PftrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pftracker.pftrackerApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PftrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PftrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pftracker/pftrackerApplication/PftrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pftracker.pftrackerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
